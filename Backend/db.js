3c2475c85298a76aff7b961f6ad971411464f3bb
Niketh23 <nikethmdcl2003@gmail.com>
2024-10-27 09:35:06 +0530

    update db schema

--- Backend/db.js
+++ Backend/db.js
@@ -14,24 +14,24 @@ mongoose.connect(process.env.URI, {
 });
 
 const userSchema = new mongoose.Schema({
-  ID: { type: Number, required: true },
-  Applicant_Name: { type: String, required: true },
-  Gender: { type: String, required: true },
-  District: { type: String, required: true },
-  State: { type: String, required: true },
-  Pincode: { type: String, required: true }, // Change to String to allow for leading zeros
-  Ownership: { type: String, required: true },
-  GovtID_Type: { type: String, required: true },
-  ID_Number: { type: String, required: true }, // Change to String for flexibility
-  Category: { type: String, required: true },
-  Load_Applied: { type: Number, required: true },
-  Date_of_Application: { type: Date, required: true },
-  Date_of_Approval: { type: Date },
-  Modified_Date: { type: Date },
-  Status: { type: String, required: true },
-  Reviewer_ID: { type: Number },
-  Reviewer_Name: { type: String },
-  Reviewer_Comments: { type: String },
+  ID: { type: Number },
+  Applicant_Name: { type: String},
+  Gender: { type: String},
+  District: { type: String},
+  State: { type: String},
+  Pincode: { type: String}, // Change to String to allow for leading zeros
+  Ownership: { type: String},
+  GovtID_Type: { type: String},
+  ID_Number: { type: String}, // Change to String for flexibility
+  Category: { type: String},
+  Load_Applied: { type: Number},
+  Date_of_Application: { type: String},
+  Date_of_Approval: { type: String},
+  Modified_Date: { type: String },
+  Status: { type: String},
+  Reviewer_ID: { type: Number},
+  Reviewer_Name: { type: String},
+  Reviewer_Comments: { type: String},
 });
 
 const Users = mongoose.model('Users', userSchema);
